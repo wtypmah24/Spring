db801f991b656c420fa644bd6b67f3dae131a04d
wtypmah24 <wtypmah24@gmail.com>
2024-02-10 17:25:14 +0100

    fix sudoku

--- untitled/src/main/java/Main.java
+++ untitled/src/main/java/Main.java
@@ -2,13 +2,12 @@ import com.sudoku.config.AppConfig;
 import com.sudoku.controler.SudokuController;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-//TODO: Make cells after initializing empty.
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         SudokuController controller = context.getBean("sudokuController", SudokuController.class);
-        controller.solveSudoku();
+        //controller.solveSudoku();
     }
 }
